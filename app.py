8ec0111a34e2d061deb273baccd618013e085a20
jonah18 <jonahmg@umich.edu>
2017-08-27 14:28:38 -0500

    Sidebar, about page, styling

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import controllers
 app = Flask(__name__, template_folder="templates")
 
 app.register_blueprint(controllers.main)
+app.register_blueprint(controllers.about)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ controllers/about.py
@@ -0,0 +1,7 @@
+from flask import *
+
+about = Blueprint('about', __name__, template_folder='templates')
+
+@about.route('/about')
+def about_route():
+    return render_template("about.html")
