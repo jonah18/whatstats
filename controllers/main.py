86ec6bb21ff7ea8b813008924f329d22bbcd347f
jonah18 <jonahmg@umich.edu>
2017-08-27 19:39:41 -0500

    Time Ranges

--- controllers/main.py
+++ controllers/main.py
@@ -1,17 +1,19 @@
 from flask import *
+from collections import OrderedDict
+from datetime import datetime, timedelta
 
 main = Blueprint('main', __name__, template_folder='templates')
 
-
-def updatePostStats(author, stats):
+# Update number of posts stats
+def calcPostStats(author, stats):
 
     if author in stats:
         stats[author] += 1
     else:
         stats[author] = 1
 
-# Update group messaging statistics
-def updateWordCountStats(author, message, stats):
+# Update number of words stats
+def calcWordStats(author, message, stats):
 
     if author not in stats:
         stats[author] = 0
@@ -23,6 +25,14 @@ def updateWordCountStats(author, message, stats):
     for _ in message:
         stats[author] += 1
 
+# Update number of media stats
+def calcMediaStats(author, message, stats):
+
+    if author not in stats:
+        stats[author] = 0
+
+    if "<" in message:
+        stats[author] += 1
 
 def parseMessage(line, chat, metric, stats):
     split = line.split(":")
@@ -37,15 +47,35 @@ def parseMessage(line, chat, metric, stats):
     message = split[4][1:]
 
     if metric == "posts":
-        updatePostStats(author, stats)
+        calcPostStats(author, stats)
 
-    else:
-        updateWordCountStats(author, message, stats)
+    elif metric == "words":
+        calcWordStats(author, message, stats)
+
+    elif metric == "media":
+        calcMediaStats(author, message, stats)
 
     chat["author"] = author
     chat["message"] = message
     chat["date"] = date
     chat["timestamp"] = timestamp + "M"
+
+    return True
+
+# If date range is set, checks if message falls within range
+def validMessageDate(line, beginDate):
+    split = line.split(":")
+
+    if len(split) < 5:
+        return False
+
+    date = line.split(",")[0]
+    splitDate = date.split("/")
+
+    dateTime = datetime(int('20' + splitDate[2]), int(splitDate[0]), int(splitDate[1]))
+
+    if dateTime < beginDate:
+        return False
 
     return True
 
@@ -59,26 +89,53 @@ def main_route():
 
     elif request.method == 'POST':
 
-        chats = []
-        stats = {}
-
-
-        metric = request.form['metric']
-        file = request.files['file']
-
         if request.files['file'].filename == '':
             options['message'] = "No File"
 
             return render_template("home.html", **options)
 
+        chats = []
+        stats = {}
+        rangeSet = False
+
+        metric = request.form['metric']
+        file = request.files['file']
+        timeUnit = request.form['time-unit']
+
+        # Check if time range is set
+        if timeUnit != "all":
+            rangeSet = True
+
+            now = datetime.now()
+            timeNumber = int(request.form['time-number'])
+
+            # Determine beginning range for date
+            if timeUnit == "year":
+                timeDelta = timedelta(days=365*timeNumber)
+
+            elif timeUnit == "month":
+                timeDelta = timedelta(days=30*timeNumber)
+
+            else:
+                timeDelta = timedelta(weeks=timeNumber)
+
+            beginDate = now - timeDelta
+
         numChats = 0
         for line in file.readlines():
             chat = {}
-            line = str(line)
+            line = line.decode('utf-8')
+
+            if rangeSet:
+                if not validMessageDate(line, beginDate):
+                    continue
 
             if parseMessage(line, chat, metric, stats):
                 chats.append(chat)
             numChats += 1
+
+        stats = OrderedDict(sorted(stats.items(), key=lambda x: x[1], reverse=True))
+
         options['message'] = "Success"
         options['stats'] = stats
 
